a54789bea1de156df286d885928d57a7dfe7f597
burset <luizaanjo2007@gmail.com>
2017-12-13 20:47:48 -0200

    Versão Carlo do Jogo 0.0.1

---
+++ doc/inca.rst
@@ -0,0 +1,13 @@
+.. SupyJogo documentation master file, created by
+   sphinx-quickstart on Wed Dec 13 19:15:26 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Classes do Jogo Inca
+====================================
+
+.. automodule:: inca
+
+:members:
+
+
---
+++ src/inca.py
@@ -0,0 +1,37 @@
+"""
+Módulo Principal do Jogo Inca
+"""
+
+
+class Jogo:
+    """
+    O Jogo Principal.
+    """
+    def __init__(self):
+        print("benvindo ao misterioso templo inca!")
+
+class Baralho:
+    """
+    Contém todas as cartas do jogo
+    """
+    ...
+
+
+class Carta:
+    """
+    Representa Artefato, Tesouro ou Perigo
+    """
+
+    ...
+
+class Tunel:
+    """
+    Parte interna do templo onde se encontra coisas boas ou más.
+    """
+
+    ...
+
+
+if __name__ == '__main__':
+    Jogo()
+
