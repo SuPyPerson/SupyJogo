1ab4dd43044300954887deb25249b469e174ea12
SrBartolomeu <eduardo.VPP7@gmail.com>
2017-12-13 20:35:54 -0200

    Versão Ed do jogo 0.0.1

---
+++ src/inca.py
@@ -0,0 +1,37 @@
+"""
+modulo principal do jogo inca
+"""
+
+
+class jogo:
+    """
+    o jogo principal
+    """
+    def __init__(self):
+        print("bem-vindo ao jogo inca")
+
+
+class baralho:
+    """
+    contém todas as cartas do jogo
+    """
+    ...
+
+
+class carta:
+    """
+    contém artefato, perigo ou tesouro
+    """
+
+    ...
+
+
+class tunel:
+    """
+    parte interna do templo onde se encontra coisas boas ou más
+    """
+    ...
+
+
+if __name__=="__main__":
+    jogo()
